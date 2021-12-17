bf2b07bf6b6d88601bf196489ee285e02b18bf78
nahyonii <95596990+nahyonii@users.noreply.github.com>
2021-12-17 21:27:02 +0900

    Add: 주석

--- sagas/index.js
+++ sagas/index.js
@@ -2,7 +2,7 @@ import { all, call } from "redux-saga/effects";
 import axios from "axios";
 import user from "./user";
 
-//서버 api 주소
+//서버 api 주소 설정 부분입니다. 사용 중인 주소로 수정하시면 됩니다.
 axios.defaults.baseURL = "http://15.165.28.19:4000/api";
 axios.defaults.withCredentials = true;
 
